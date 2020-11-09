21393306dc5fd0dfd5256e00d5456491829cae67
apainintheneck <apainintheneck@gmail.com>
2020-11-09 19:01:21 +0000

    Working on layout.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*global $*/
+$(document).ready(function(){
+    var filterStyles = {
+        "blur": "",
+        "brightness": "",
+        "contrast": "",
+        "grayscale": "",
+        "hue-rotate": "",
+        "invert": "",
+        "opacity": "",
+        "saturate": "",
+        "sepia": "",
+    };
+})
